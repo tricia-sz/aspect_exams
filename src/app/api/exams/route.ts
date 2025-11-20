865c4c4007d2095afdce2ec28675da51372250d9
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-20 13:01:56 -0300

    get findMany exams

--- src/app/api/exams/route.ts
+++ src/app/api/exams/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse } from "next/server";
-import {prisma} from 
-export async function GET(){
-  const exams = await 
+import { NextResponse } from 'next/server';
+import { prisma } from '../../../lib/prisma';
+
+export async function GET() {
+  const exams = await prisma.exam.findMany();
+  return NextResponse.json(exams);
 }
