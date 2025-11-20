503f2f4a9610b2a357ce80ecb5546a47be0c282f
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-20 19:43:24 -0300

    Card Appointment and Exams

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { prisma } from '../../lib/prisma';
+import { Card } from '@/components/ui/card';
 
 export default async function AppointmentPage() {
   const appointments = await prisma.appointment.findMany({
@@ -8,13 +9,13 @@ export default async function AppointmentPage() {
   });
 
   return (
-    <div className="max-w-3xl mx-auto py-10">
+    <Card className="max-w-3xl p-6 mt-10 mx-auto rounded-xl shadow-2xl shadow-accent-primary border-none">
       <div className="flex justify-between mb-6">
         <h1 className="text-3xl font-bold">Agendamentos</h1>
 
         <Link
           href="/appointment/new"
-          className="bg-accent-primary text-white px-4 py-2 rounded-full"
+          className="bg-accent-primary text-white px-4 py-2 rounded-md hover:bg-accent-blue shadow-sm hover:shadow-md transition  shadow-accent-primary"
         >
           + Novo Agendamento
         </Link>
@@ -25,10 +26,12 @@ export default async function AppointmentPage() {
           <Link
             key={a.id}
             href={`/appointment/${a.id}`}
-            className="block border p-4 rounded hover:bg-gray-100"
+            className="block rounded-lg p-4 shadow-sm hover:shadow-md transition  shadow-accent-primary "
           >
             <h2 className="font-bold">{a.exam.examName}</h2>
-            <p>📅 {new Date(a.scheduledAt).toLocaleString('pt-BR')}</p>
+            <p className="text-content-secondary py-2">
+              📅 {new Date(a.scheduledAt).toLocaleString('pt-BR')}
+            </p>
             {a.additionalInformation && (
               <p className="text-sm text-gray-600">
                 🔎 {a.additionalInformation}
@@ -37,6 +40,6 @@ export default async function AppointmentPage() {
           </Link>
         ))}
       </div>
-    </div>
+    </Card>
   );
 }
