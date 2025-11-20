01c19ff4da2e6baa67b34f0ff1d7654891cb7b51
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-20 18:53:50 -0300

    Page grid app and exams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import AppointmentPage from './appointment/page';
 import ExamPage from './exam/page';
-export default async function Home() {
+import AppointmentPage from './appointment/page';
+
+export default function Home() {
   return (
-    <div>
-      <ExamPage />
-      <AppointmentPage />
+    <div className="max-w-7xl mx-auto py-10 px-4">
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        Exames & Agendamentos
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="rounded-xl shadow-2xl p-6 shadow-accent-primary">
+          <ExamPage />
+        </div>
+        <div className="rounded-xl shadow-2xl shadow-accent-primary p-6">
+          <AppointmentPage />
+        </div>
+      </div>
     </div>
   );
 }
