0244f2875a0a246db4275393ca46b55f39633f44
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-20 13:27:04 -0300

    Testing get exams in interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { prisma } from '../lib/prisma';
+
+export default async function Home() {
+  const exams = await prisma.exam.findMany();
   return (
     <div>
-      <h1>Aspect Home Page</h1>
+      <h1>GET dos exames</h1>
+      <p>{JSON.stringify(exams, null, 3)}</p>
     </div>
   );
 }
