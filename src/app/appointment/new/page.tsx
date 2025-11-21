7870b131f873e34f15463743c2d13c158437d57b
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-21 03:12:30 -0300

    Deploy ajustment new page

--- src/app/appointment/new/page.tsx
+++ src/app/appointment/new/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { redirect } from 'next/navigation';
 import { prisma } from '../../../lib/prisma';
 import { Textarea } from '@/components/ui/textarea';
