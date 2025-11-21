29b3dffb50aea6bea0f53087453eb0222fae0ff3
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-21 03:25:12 -0300

    Deploy - E lá vamos nós 999

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
 import ExamPage from './exam/page';
 import AppointmentPage from './appointment/page';
 
